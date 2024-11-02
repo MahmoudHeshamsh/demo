be949f78301c602f2640bca5558d00952491c6cc
Mahmoud Hesham <mshalash689@gmail.com>
2024-11-02 15:54:31 +0200

    Finish task editing 2

--- lib/tabs/tasks/tasks.dart
+++ lib/tabs/tasks/tasks.dart
@@ -30,81 +30,183 @@ class _TasksState extends State<Tasks> {
     ThemeData theme = Theme.of(context);
     return Container(
       margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 20),
-      child: Slidable(
-        startActionPane: ActionPane(
-          motion: const ScrollMotion(),
-          dismissible: DismissiblePane(onDismissed: () {}),
-          children: [
-            SlidableAction(
-              onPressed: (_) {
-                FirebaseFunctions.deleteTaskFromFirestore(widget.task.id)
-                    .timeout(Duration(milliseconds: 100), onTimeout: () {
-                  Provider.of<TasksProvider>(context,listen: false).getTasks();
-                }).catchError((error){
-                  Fluttertoast.showToast(
-                    msg: "Something wents wrong",
-                    toastLength: Toast.LENGTH_LONG,
-                    timeInSecForIosWeb: 5,
-                    backgroundColor: Colors.grey,
-                    textColor: Colors.white,
-                    fontSize: 16.0,
-                );
-                });
-              },
-              backgroundColor: AppTheme.red,
-              foregroundColor: AppTheme.white,
-              icon: Icons.delete,
-              label: 'Delete',
+      child: Column(
+        children: [
+          Slidable(
+            key: ValueKey(widget.task.id.hashCode),
+            startActionPane: ActionPane(
+              motion: const ScrollMotion(),
+              dismissible: DismissiblePane(onDismissed: () {}),
+              children: [
+                SlidableAction(
+                  onPressed: (_) {
+                    FirebaseFunctions.deleteTaskFromFirestore(widget.task.id)
+                        .timeout(Duration(milliseconds: 100), onTimeout: () {
+                      Provider.of<TasksProvider>(context, listen: false)
+                          .getTasks();
+                    }).catchError((error) {
+                      Fluttertoast.showToast(
+                        msg: "Something went wrong",
+                        toastLength: Toast.LENGTH_LONG,
+                        timeInSecForIosWeb: 5,
+                        backgroundColor: Colors.grey,
+                        textColor: Colors.white,
+                        fontSize: 16.0,
+                      );
+                    });
+                  },
+                  backgroundColor: AppTheme.red,
+                  foregroundColor: AppTheme.white,
+                  icon: Icons.delete,
+                  label: 'Delete',
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      bottomLeft: Radius.circular(10)),
+                ),
+                SlidableAction(
+                  onPressed: (_) {
+                    setState(() {
+                      isEditActive = !isEditActive;
+                      newTitleController.text =
+                          widget.task.title; 
+                      newDescriptionController.text = widget.task.description;
+                    });
+                  },
+                  backgroundColor: AppTheme.gray,
+                  foregroundColor: AppTheme.white,
+                  icon: Icons.edit,
+                  label: 'Edit',
+                  borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(10),
+                      bottomRight: Radius.circular(10)),
+                ),
+              ],
             ),
-          ],
-        ),
-        child: Container(
-          padding: const EdgeInsets.all(20),
-          decoration: BoxDecoration(
-              color: AppTheme.white, borderRadius: BorderRadius.circular(15)),
-          child: Row(
-            children: [
-              Container(
-                margin: const EdgeInsetsDirectional.only(end: 8),
-                decoration: BoxDecoration(
-                    color: theme.primaryColor,
-                    borderRadius: BorderRadius.circular(10)),
-                height: 62,
-                width: 4,
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+            child: Container(
+              padding: const EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                  color: AppTheme.white,
+                  borderRadius: BorderRadius.circular(15)),
+              child: Row(
                 children: [
-                  Text(
-                    widget.task.title,
-                    style: theme.textTheme.titleMedium
-                        ?.copyWith(color: AppTheme.primary),
+                  Container(
+                    margin: const EdgeInsetsDirectional.only(end: 8),
+                    decoration: BoxDecoration(
+                        color: theme.primaryColor,
+                        borderRadius: BorderRadius.circular(10)),
+                    height: 62,
+                    width: 4,
                   ),
-                  const SizedBox(
-                    height: 4,
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        widget.task.title,
+                        style: theme.textTheme.titleMedium
+                            ?.copyWith(color: AppTheme.primary),
+                      ),
+                      const SizedBox(height: 4),
+                      Text(
+                        widget.task.description,
+                        style: theme.textTheme.titleSmall,
+                      ),
+                    ],
                   ),
-                  Text(
-                    widget.task.description,
-                    style: theme.textTheme.titleSmall,
-                  )
+                  const Spacer(),
+                  Container(
+                    decoration: BoxDecoration(
+                        color: AppTheme.primary,
+                        borderRadius: BorderRadius.circular(10)),
+                    width: 69,
+                    height: 34,
+                    child: const Icon(
+                      Icons.check,
+                      size: 32,
+                      color: AppTheme.white,
+                    ),
+                  ),
                 ],
               ),
-              const Spacer(),
-              Container(
-                decoration: BoxDecoration(
-                    color: AppTheme.primary,
-                    borderRadius: BorderRadius.circular(10)),
-                width: 69,
-                height: 34,
-                child: const Icon(
-                  Icons.check,
-                  size: 32,
-                  color: AppTheme.white,
-                ),
-              )
-            ],
+            ),
           ),
-        ),
+          if (isEditActive)
+            Container(
+              margin: EdgeInsets.only(top: 10),
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                  color: AppTheme.white,
+                  borderRadius: BorderRadius.circular(10)),
+              child: Column(
+                children: [
+                  ComTextFormField(
+                    controller: newTitleController,
+                    hintText: 'Edit Task',
+                    validator: (value) {
+                      if (value == null || value.trim().isEmpty) {
+                        return 'Please enter a task';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 8),
+                  ComTextFormField(
+                    controller: newDescriptionController,
+                    hintText: 'Edit Description',
+                    validator: (value) {
+                      if (value == null || value.trim().isEmpty) {
+                        return 'Please enter a description';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 8),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      ComElevatedButton(
+                        label: 'Save',
+                        onPressed: () {
+                          FirebaseFunctions.updateTaskFromFirestore(
+                            widget.task.id,
+                            newTitleController.text,
+                            newDescriptionController.text,
+                          ).timeout(
+                            Duration(microseconds: 100),
+                            onTimeout: () {
+                              setState(() {
+                                widget.task.title = newTitleController.text;
+                                widget.task.description =
+                                    newDescriptionController.text;
+                                isEditActive = !isEditActive;
+                                Provider.of<TasksProvider>(context, listen: false)
+                          .getTasks();
+                              });
+                            },
+                          ).catchError((error) {
+                            Fluttertoast.showToast(
+                              msg: "Failed to update task",
+                              toastLength: Toast.LENGTH_LONG,
+                              backgroundColor: Colors.red,
+                              textColor: Colors.white,
+                            );
+                          });
+                        },
+                      ),
+                      ComElevatedButton(
+                        label: 'Cancel',
+                        onPressed: () {
+                          setState(() {
+                            isEditActive = !isEditActive;
+                          });
+                        },
+                        color: AppTheme.red,
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+        ],
       ),
     );
   }
